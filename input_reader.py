9dca925c14e9fd2806a13ef44f629f99ac764fc6
CarlRazmus <carl.razmus@outlook.com>
2022-11-20 21:44:12 +0100

    rewrite/fix faulty regex functions

--- input_reader.py
+++ input_reader.py
@@ -9,10 +9,6 @@ AOC_URL = "https://adventofcode.com/2021/day"
 def puzzle_data_exists(day):
     return f"day_{day}.txt" in os.listdir("inputs")
 
-def read_test_input():
-    with open("inputs/test_data.txt", encoding="UTF-8") as file:
-        return file.read()
-
 def create_puzzle_data(day):
     print(f"creating new puzzle data for day {day}")
     jar = requests.cookies.RequestsCookieJar()
@@ -21,6 +17,10 @@ def create_puzzle_data(day):
     if response.ok:
         with open(f"inputs/day_{day}.txt", "w", encoding="UTF-8") as file:
             file.write(response.text)
+
+def read_test_input():
+    with open("inputs/test_data.txt", encoding="UTF-8") as file:
+        return [line.rstrip('\n') for line in file]
 
 def read_input():
     calling_file = [stack.filename for stack in inspect.stack() if "AdventOfCode" in stack.filename][-1]
@@ -32,14 +32,21 @@ def read_input():
     with open(f"inputs/day_{day}.txt", "r", encoding="UTF-8") as file:
         return [line.rstrip('\n') for line in file]
 
-def get_input_as_int(read_func=read_input):
+def get_as_int(read_func=read_input):
     return [int(x) for x in read_func()]
 
-def get_input_as_string(read_func=read_input):
+def get_as_ints(read_func=read_input, split_cond=" "):
+    return [[int(y) for y in x.split(split_cond)] for x in read_func()]
+
+def get_as_string(read_func=read_input):
     return read_func()
 
-def get_input_with_regex(expr, occurence=0, read_func=read_input):
-    return [re.search(expr, x).group(occurence) for x in read_func()]
+def get_as_strings(read_func=read_input, split_cond=" "):
+    return [[y for y in x.split(split_cond)] for x in read_func()]
 
-def get_input_with_regex_groups(expr, read_func=read_input):
-    return [re.search(expr, x).groups for x in read_func()]
+def get_with_regex(expr, group_idx=0, read_func=read_input):
+    #return [re.search(expr, x).group(group_idx) for x in read_func()]
+    return [m.group(group_idx) for l in read_func() for m in [re.search(expr, l)] if m]
+
+def get_with_regex_groups(expr, read_func=read_input):
+    return [m.groups() for l in read_func() for m in [re.search(expr, l)] if m]
