12fdc3f98f318fbd55cf43f96e8a72a6af22810e
CarlRazmus <carl.razmus@outlook.com>
2022-12-01 06:51:30 +0100

    solution of day 1

--- day_1.py
+++ day_1.py
@@ -1,3 +1,6 @@
-import input_reader
+import input_reader as ir
 
-input_reader.get_input()
+if __name__ == "__main__":
+    l = [sum(ir.get_as_ints(g)) for g in ir.get_with_separator("\n\n")]
+    print(max(l))
+    print(sum(sorted(l)[-3:]))
