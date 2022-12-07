3fa7e84d0469fb0144cabce281dbece62310d843
CarlRazmus <carl.razmus@outlook.com>
2022-12-07 22:05:54 +0100

    solution of day 7

--- day_7.py
+++ day_7.py
@@ -1,11 +1,8 @@
 import input_reader as ir
-import re
-import sys
-
-valid_commands = ["cd", "dir", ]
 
 
 class Directory():
+
     def __init__(self, name):
         self.name = name
         self.files = []
@@ -13,47 +10,52 @@ class Directory():
 
     def get_size(self):
         tot_size = 0
-        for name, size in self.files:
+        for _, size in self.files:
             tot_size = tot_size + size
         for ddd in self.subdirs:
             tot_size = tot_size + d[ddd].get_size()
         return tot_size
 
-
-
+def get_dir_name():
+    if len(dirs_stack) > 1:
+        return "/".join(dirs_stack)
+    elif len(dirs_stack) == 1:
+        return dirs_stack[0]
+    else:
+        return ""
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(3000)
     i = ir.read_input_lines()
+    current_dir = ""
     d = {}
     dirs_stack = list()
     for line in i:
         command_line = line.split()
-
         if command_line[0] == "$":
             if command_line[1] == "cd":
-                next_dir = line.split()[2]
-                if next_dir == "..":
+                next_catalog = command_line[2]
+                if next_catalog == "..":
                     dirs_stack.pop()
+                    current_dir = get_dir_name()
                 else:
+                    current_dir = get_dir_name()
+                    dirs_stack.append(next_catalog)
+                    next_dir = get_dir_name()
                     if next_dir not in d:
                         d[next_dir] = Directory(next_dir)
-                    dirs_stack.append(next_dir)
+                    if current_dir != "":
+                      d[current_dir].subdirs.append(next_dir)
+                    current_dir = next_dir
         else:
             size, file_name = line.split()
-            current_dir = dirs_stack[-1]
             if size != "dir":
                 existing_names = [n for n,_ in d[current_dir].files]
                 if file_name not in existing_names:
                     d[current_dir].files.append((file_name, int(size)))
-            else:
-                d[current_dir].subdirs.append(file_name)
 
-
-    #print(d)
-    print("hej")
-    sizes = [d[x] for x in d if d[x].get_size() < 100000]
-    #for dname in d:
-    #    print(d[dname].get_size())
-    print(sizes)
-    print(len(sizes))
+    sizes = [d[x].get_size() for x in d]
+    small_sizes = [s for s in sizes if s < 100000]
+    print(sum(small_sizes))
+    space_needed = d["root"].get_size() - 40000000
+    big_sizes = [s for s in sizes if s >= space_needed]
+    print(sorted(big_sizes)[0])
