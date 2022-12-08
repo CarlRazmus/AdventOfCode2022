eb8825610be852d9b50f1a880f91d5624575b34c
CarlRazmus <carl.razmus@outlook.com>
2022-12-08 18:06:13 +0100

    improvement of day 8

--- day_8.py
+++ day_8.py
@@ -1,70 +1,56 @@
 import input_reader as ir
 
 
-def find_visible_trees_from_all_directions():
-    visible_trees = set()
-    #NORTH
-    for x in range(len_x):
-        previous_tree = -1
-        for y in range(len_y):
-            if forest[x][y] > previous_tree:
-                visible_trees.add((x,y))
-                previous_tree = forest[x][y]
-    #WEST
-    for y in range(len_y):
-        previous_tree = -1
-        for x in range(len_x):
-            if forest[x][y] > previous_tree:
-                visible_trees.add((x,y))
-                previous_tree = forest[x][y]
-    #SOUTH
-    for x in range(len_x):
-        previous_tree = -1
-        for y in reversed(range(len_y)):
-            if forest[x][y] > previous_tree:
-                visible_trees.add((x,y))
-                previous_tree = forest[x][y]
-    #EAST
-    for y in range(len_y):
-        previous_tree = -1
-        for x in reversed(range(len_x)):
-            if forest[x][y] > previous_tree:
-                visible_trees.add((x,y))
-                previous_tree = forest[x][y]
+def get_scenic_value(x, y, max_height):
+    tot = max(1, len(visible_trees_in_direction2(x, y, 0, 1, max_height))) #NORTH
+    tot = tot * max(1, len(visible_trees_in_direction2(x, y, -1, 0, max_height))) #WEST
+    tot = tot * max(1, len(visible_trees_in_direction2(x, y, 0, -1, max_height))) #SOUTH
+    tot = tot * max(1, len(visible_trees_in_direction2(x, y, 1, 0, max_height))) #EAST
+    return tot
 
-def visible_trees_in_direction(x, y, x_inc, y_inc, max_val):
-    scenic_value = 0
+def visible_trees_in_direction1(x, y, x_inc, y_inc):
+    visible_trees_in_direction = []
+    visible_trees_in_direction.append((x,y))
+    max_height = forest[y][x]
     x = x + x_inc
     y = y + y_inc
     while x in range(0, len_x) and y in range(0, len_y):
         val = forest[y][x]
-        scenic_value = scenic_value + 1
-        if val >= max_val:
+        if val > max_height:
+            visible_trees_in_direction.append((x,y))
+            max_height = val
+        x = x + x_inc
+        y = y + y_inc
+    return visible_trees_in_direction
+
+def visible_trees_in_direction2(x, y, x_inc, y_inc, max_height):
+    visible_trees_in_direction = []
+    x = x + x_inc
+    y = y + y_inc
+    while x in range(0, len_x) and y in range(0, len_y):
+        val = forest[y][x]
+        visible_trees_in_direction.append((x,y))
+        if val >= max_height:
             break
         x = x + x_inc
         y = y + y_inc
-    return scenic_value if scenic_value > 0 else 1
-
-def get_scenic_value(start_x, start_y, max_height):
-    #NORTH
-    tot = visible_trees_in_direction(start_x, start_y, 0, 1, max_height)
-    #WEST
-    tot = tot * visible_trees_in_direction(start_x, start_y, -1, 0, max_height)
-    #SOUTH
-    tot = tot * visible_trees_in_direction(start_x, start_y, 0, -1, max_height)
-    #EAST
-    tot = tot * visible_trees_in_direction(start_x, start_y, 1, 0, max_height)
-    return tot
+    return visible_trees_in_direction
 
 if __name__ == "__main__":
     forest = [[int(s) for s in line] for line in ir.read_input_lines()]
     len_x = len(forest[0])
     len_y = len(forest)
-    print(len(find_visible_trees_from_all_directions()))
+    visible_trees = set()
+    for x in range(len_x):
+        visible_trees.update(visible_trees_in_direction1(x, 0, 0, 1)) #top
+        visible_trees.update(visible_trees_in_direction1(x, len_y-1, 0, -1)) #bottom
+    for y in range(len_y):
+        visible_trees.update(visible_trees_in_direction1(0, y, 1, 0)) #left side
+        visible_trees.update(visible_trees_in_direction1(len_x-1, y, -1, 0)) #right side
+    print("part1: ", len(visible_trees))
 
     scenic_values = []
     for y in range(len_y):
         for x in range(len_x):
             scenic_values.append(get_scenic_value(x, y, forest[y][x]))
-    print(max(scenic_values))
-
+    print("part2: ", max(scenic_values))
