ba0a548930538611868a9e6ad38f52c9d1633814
Razmus Lindgren <razmus.lindgren@attentec.se>
2023-01-07 16:03:36 +0100

    solution of part 2 of day 21

--- day_21.py
+++ day_21.py
@@ -24,26 +24,6 @@ class Monkey:
             val2 = monkeys[self.right_operand].shout()
             return (val1, val2)
 
-#def reduce_monkeys(monkeys):
-#    reduced = True
-#    reduced_monkeys = dict(monkeys)
-#    while reduced:
-#        reduced = False
-#        for name, monkey in monkeys.items():
-#            if monkey.value is not None:
-#                continue
-#            if name == "humn":
-#                continue
-#            left = monkeys[monkey.left_operand].value
-#            right = monkeys[monkey.right_operand].value
-#            if left is not None and right is not None:
-#                reduced = True
-#                monkey.value = monkey.shout()
-#                reduced_monkeys.pop(monkey.left_operand)
-#                reduced_monkeys.pop(monkey.right_operand)
-#        monkeys = dict(reduced_monkeys)
-#    return reduced_monkeys
-
 if __name__ == "__main__":
     monkey_inputs = ir.read_input_lines()
     monkeys = {}
@@ -59,32 +39,29 @@ if __name__ == "__main__":
 
     #part 2
     monkeys["root"].operator = "="
-    start_val = monkeys["humn"].value
-    original_left, original_right = monkeys["root"].shout()
-    original_diff = abs(original_right - original_left)
-
-    #find scaling constant
-    monkeys["humn"].value = start_val + 1
-    left, right = monkeys["root"].shout()
-    diff = abs(left - right)
-    print("diff", diff)
-    print(original_diff - diff)
-    scaling_factor = int(original_diff / int((original_diff - diff)))
-    print("scaling_factor", scaling_factor)
-
-    #try to set it to a reasonable value since the scaling is constant
-    monkeys["humn"].value = start_val + scaling_factor
-
-    previous_diff = 0
     while True:
-        monkeys["humn"].value = monkeys["humn"].value - 1
+        original_left, original_right = monkeys["root"].shout()
+        original_diff = abs(original_right - original_left)
+
+        #find scaling constant
+        start_val = monkeys["humn"].value
+        monkeys["humn"].value = start_val + 1
         left, right = monkeys["root"].shout()
-        #print("left", left, "right", right)
         diff = abs(left - right)
-        print(diff)
-        if left == right:
+        print("diff", diff)
+        print(original_diff - diff)
+        scaling_factor = int(diff  / (original_diff - diff))
+        print("scaling_factor", scaling_factor)
+
+        #try to set it to a reasonable value since the scaling is ~constant
+        monkeys["humn"].value = start_val + scaling_factor
+
+        #verify if estimation is close enough to desired shout value
+        left, right = monkeys["root"].shout()
+        diff = abs(left - right)
+        print("current diff ", diff, "\n")
+        if abs(left - right) == 0:
+            print(monkeys["humn"].value)
             break
-        print("diff from last iteration", previous_diff - diff)
-        previous_diff = diff
 
     print(monkeys["humn"].value)
